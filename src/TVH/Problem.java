0194f0b426d9cbafd5e7ba45fcc1590a857b920a
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2018-11-22 13:18:51 +0100

    nieuw record 4609 na 10m

--- src/TVH/Problem.java
+++ src/TVH/Problem.java
@@ -244,9 +244,9 @@ public class Problem {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Solution best = init;
+        //Solution best = init;
 
-        //Solution best = simulatedAnnealingJeroen(1000*60*20, 20, 8, 2, 1);
+        Solution best = simulatedAnnealingJeroen(1000*60*10, 20, 8, 2, 1);
         //best.loadSolution();
         //System.out.println("start second annealing");
         //best= simulatedAnnealingJeroen(20000,100, Integer.MAX_VALUE,1);
